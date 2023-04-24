80463dd6bd12a541d884fcfefe016a742619fadb
Leonardo Batista <leonardo.de.souza.batista@gmail.com>
2023-04-23 22:02:59 -0300

    tchau

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\TrustProxies::class
+        \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class
     ];
 
     /**
