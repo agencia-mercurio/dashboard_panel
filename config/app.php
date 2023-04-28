90129e0316012b9991f5620f3b5070eded297d3a
Leonardo Batista <leonardo.de.souza.batista@gmail.com>
2023-04-28 15:43:43 -0300

    production

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Fruitcake\Cors\CorsServiceProvider::class,
 
 
         /*
